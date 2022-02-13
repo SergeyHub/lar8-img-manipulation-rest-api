59cb651671e4203c27fcf1fc364f6c871997427c
Sergey <scoder127@gmail.com>
2022-02-14 04:27:51 +0500

    2. Add album model, migration and implement CRUD

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\V1\AlbumController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function(){
+    Route::apiResource('album', AlbumController::class);
+});
+
